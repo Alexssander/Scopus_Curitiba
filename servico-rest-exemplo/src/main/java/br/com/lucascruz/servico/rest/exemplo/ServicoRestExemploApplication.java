7fe9f0bd3df890a0a73bfbea3cf0f927771ce542
ldealmei <lukita.a@gmail.com>
2016-07-06 13:30:32 -0300

    primeiro commit do server

---
+++ servico-rest-exemplo/src/main/java/br/com/lucascruz/servico/rest/exemplo/ServicoRestExemploApplication.java
@@ -0,0 +1,12 @@
+package br.com.lucascruz.servico.rest.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoRestExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoRestExemploApplication.class, args);
+	}
+}
